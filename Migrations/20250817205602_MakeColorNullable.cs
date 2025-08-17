f3a8d8c6c980902457425ae52478e8a7dec673a4
Your Name <you@example.com>
2025-08-17 16:11:14 -0500

    Feat: Solucionar configuracion "nullable" en migraciones bd al momento de ejecutar el endpoint de apostar donde en el body permite enviar como parametro solo el monto a apostar y el numero a gusto propio. Con color ya estaba estable, QA regresion Passed.

---
+++ Migrations/20250817205602_MakeColorNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JEMP_API_HeyGIA.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeColorNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Color",
+                table: "Apuestas",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Color",
+                table: "Apuestas",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
--- Modelos/Apuesta.cs
+++ Modelos/Apuesta.cs
@@ -7,7 +7,7 @@
         public Ruleta Ruleta { get; set; }
         public string UsuarioId { get; set; } // desde headers
         public int? Numero { get; set; } // 0-36
-        public string Color { get; set; } // "rojo" o "negro"
+        public string? Color { get; set; } // "rojo" o "negro"
         public decimal Monto { get; set; }
     }
 
